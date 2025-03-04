9e551d4e8ec5255fe55875ab251252811a2cca65
byjsv <1274804935@qq.com>
2025-03-04 14:14:00 +0800

    update

--- AIR780E/Air780E.h
+++ AIR780E/Air780E.h
@@ -9,4 +9,18 @@ uint8_t AT_SendCommand(char* command,char *Response, uint16_t timeout, uint8_t R
 uint8_t AT_SendCommand_2(char* command,char *Response, char *Response2, uint16_t timeout, uint8_t Retry) ;
 void AIR780E_Init(void);
 
+uint8_t AT_WaitResponse(char *Response, uint16_t timeout, uint8_t Retry) ;
+void Sys_Soft_Reset(void);
+
+void errorLog(void);
+
+void AT_TCPConnect(char *TCPServer,char* Port);
+
+void AT_TCPSendData(char* send_buf);
+void set_AImodel(const char* model,const char* key,const char* url);
+void Air780E_AIRequest(char* userContent, char* sysContent);
+char* Air780E_AIRecive(char* userContent, char* sysContent);
+
+
+
 #endif
